7d97524b9e9cd9cd8f4a57d6dfd111e88e1394e3
Tim Ebbeke <Tim06TR@gmail.com>
2022-05-07 00:16:18 +0200

    Doxygen build is now disableable.

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -5,10 +5,16 @@ if [[ $CURDIR == "scripts" ]]; then
   cd ..
 fi
 
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/build_overrides.sh"
+
 export BUILD_DOCS=on
 
 ./scripts/build.sh
 
 cd ./build/clang_debug
-cmake --build . --target doxygen
+if [ -z ${DISABLE_DOXYGEN_BUILD+x} ]; then
+    cmake --build . --target doxygen
+fi
 cmake --build . --target documentation
