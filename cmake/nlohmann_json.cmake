5ae16219d7103c15c22cee0c16876c05ef0f5ed2
Tim Ebbeke <Tim06TR@gmail.com>
2022-05-17 01:42:43 +0200

    Using nlohmann json from git.

--- cmake/nlohmann_json.cmake
+++ cmake/nlohmann_json.cmake
@@ -1,2 +1,10 @@
-# TODO: use fetch content
-find_package(nlohmann_json REQUIRED)
+project(nlohmann_json-git NONE)
+
+include(FetchContent)
+FetchContent_Declare(
+    nlohmann_json
+    GIT_REPOSITORY https://github.com/nlohmann/json.git
+    GIT_TAG        a8a547d7a212a6a39943bbd5b4220be504a1a33e
+)
+
+FetchContent_MakeAvailable(nlohmann_json)
