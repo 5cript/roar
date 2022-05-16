66911516f0783f0cf6e243510fbcd02744e29201
Tim Ebbeke <Tim06TR@gmail.com>
2022-05-17 01:42:55 +0200

    Disabling example builds.

--- cmake/xhawk18_promise.cmake
+++ cmake/xhawk18_promise.cmake
@@ -13,5 +13,5 @@ if (WIN32)
   # MS SOCK
   target_link_libraries(promise PUBLIC -lws2_32 -lmswsock -lbcrypt)
 
-  target_compile_definitions(promise PUBLIC -DPROMISE_MULTITHREAD=1)
+  target_compile_definitions(promise PUBLIC -DPROMISE_MULTITHREAD=1 -DPROMISE_BUILD_EXAMPLES=off)
 endif()
