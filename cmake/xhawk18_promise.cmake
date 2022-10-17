f08b8f4ca504c0efaa94339c56edc0c07fbb3d29
Tim Ebbeke <tim06tr@gmail.com>
2022-10-18 00:23:05 +0200

    Fixed wrong setting of cmake variables.

--- cmake/xhawk18_promise.cmake
+++ cmake/xhawk18_promise.cmake
@@ -1,5 +1,7 @@
 project(promise-git NONE)
 
+set(PROMISE_BUILD_EXAMPLES off CACHE BOOL "Promise build examples" FORCE)
+set(PROMISE_MULTITHREAD on CACHE BOOL "Promise multithreading on")
 include(FetchContent)
 FetchContent_Declare(
     promise
@@ -12,6 +14,4 @@ FetchContent_MakeAvailable(promise)
 if (WIN32)
   # MS SOCK
   target_link_libraries(promise PUBLIC -lws2_32 -lmswsock -lbcrypt)
-
-  target_compile_definitions(promise PUBLIC -DPROMISE_MULTITHREAD=1 -DPROMISE_BUILD_EXAMPLES=off)
 endif()
