33b01f8bfdadf0038deebd03d27d85c2d2794fbe
Tim Ebbeke <tim06tr@gmail.com>
2022-10-18 00:14:16 +0200

    Updated nlohmann json.

--- cmake/nlohmann_json.cmake
+++ cmake/nlohmann_json.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     nlohmann_json
     GIT_REPOSITORY https://github.com/nlohmann/json.git
-    GIT_TAG        a8a547d7a212a6a39943bbd5b4220be504a1a33e
+    GIT_TAG        9dfa7226693012ed5bcf5ab3bc5d8e69d58006ab
 )
 
 FetchContent_MakeAvailable(nlohmann_json)
