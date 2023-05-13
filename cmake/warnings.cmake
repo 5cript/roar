c509e1780cf485c4851579d9c7e2841bbcbc5fa0
Tim Ebbeke <Tim06TR@gmail.com>
2023-05-14 00:29:06 +0200

    Improved warnings.

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -1,8 +1,18 @@
 function(set_target_warnings project_name)
-    set(CLANG_WARNINGS
+    if (${MSVC})
+        set(CLANG_WARNINGS -Wmost)
+        list(APPEND CLANG_WARNINGS -Wextra)
+        set(GCC_WARNINGS -Wall)
+    else()
+        set(CLANG_WARNINGS -Wall)
+        list(APPEND CLANG_WARNINGS -Wextra)
+        set(GCC_WARNINGS -Wall)
+    endif()
+
+    list(APPEND CLANG_WARNINGS
         -Wshadow # warn the user if a variable declaration shadows one from a parent context
         -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual
-                            # destructor. This helps catch hard to track down memory errors
+                        # destructor. This helps catch hard to track down memory errors
         -Wold-style-cast # warn for c-style casts
         -Wcast-align # warn for potential performance problem casts
         -Wunused # warn on anything being unused
@@ -12,17 +22,10 @@ function(set_target_warnings project_name)
         -Wsign-conversion # warn on sign conversions
         -Wnull-dereference # warn if a null dereference is detected
         -Wdouble-promotion # warn if float is implicit promoted to double
-        -Wformat=2 # warn on security issues around functions that format output (ie printf)
     )
 
-    if (${MSVC})
-        set(CLANG_WARNINGS "${CLANG_WARNINGS} -Wmost")
-    else()
-        set(CLANG_WARNINGS "${CLANG_WARNINGS} -Wall -Wextra")
-    endif()
-
-    set(GCC_WARNINGS
-        -Wall
+    list(  
+        APPEND GCC_WARNINGS
         # -Wshadow is too aggressive on gcc and does not understand lambdas well
         -Wold-style-cast
         -Wunused
@@ -33,18 +36,18 @@ function(set_target_warnings project_name)
         -Wdouble-promotion)
 
     if(WARNINGS_AS_ERRORS)
-        set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
-        set(GCC_WARNINGS ${GCC_WARNINGS} -Werror)
+        list(CLANG_WARNINGS APPEND -Werror)
+        list(GCC_WARNINGS APPEND -Werror)
     endif()
 
     if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-        set(PROJECT_WARNINGS ${CLANG_WARNINGS})
+        set(WARNING_SWITCHES ${CLANG_WARNINGS})
     elseif(CMAKE_CXX_COMPILER_ID MATCHES ".*GNU")
-        set(PROJECT_WARNINGS ${GCC_WARNINGS})
+        set(WARNING_SWITCHES ${GCC_WARNINGS})
     else()
         message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
     endif()
 
-    target_compile_options(${project_name} INTERFACE ${PROJECT_WARNINGS})
+    target_compile_options(${project_name} PRIVATE ${WARNING_SWITCHES})
 
 endfunction()
