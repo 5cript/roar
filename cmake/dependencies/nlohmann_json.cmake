13ad88b611357035c2d781082d849b7a753b790e
5cript <tim06tr@gmail.com>
2024-07-07 04:31:46 +0200

    Update nlohmann json

--- cmake/dependencies/nlohmann_json.cmake
+++ cmake/dependencies/nlohmann_json.cmake
@@ -1,6 +1,6 @@
 option(ROAR_EXTERNAL_NLOHMANN_JSON "Use an external nlohmann_json library (provide it manually)" OFF)
 set(ROAR_NLOHMANN_JSON_GIT_REPOSITORY "https://github.com/nlohmann/json.git" CACHE STRING "The URL from which to clone the nlohmann_json repository")
-set(ROAR_NLOHMANN_JSON_GIT_TAG "9dfa7226693012ed5bcf5ab3bc5d8e69d58006ab" CACHE STRING "The git tag or commit hash to checkout from the nlohmann_json repository")
+set(ROAR_NLOHMANN_JSON_GIT_TAG "8c391e04fe4195d8be862c97f38cfe10e2a3472e" CACHE STRING "The git tag or commit hash to checkout from the nlohmann_json repository")
 
 if (ROAR_EXTERNAL_NLOHMANN_JSON)
 else()
